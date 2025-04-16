c4dd67e50f56f4616293ca499ba8105ab6b31eee
laurab04-design <laura_b_04@hotmail.com>
2025-04-16 15:25:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -184,7 +184,7 @@ if __name__ == "__main__":
     urls = fetch_aspx_links()
 if not urls:
     print("No show URLs found.")
-    return links
+    return
     
     travel_cache = {}
     if os.path.exists(CACHE_FILE):
