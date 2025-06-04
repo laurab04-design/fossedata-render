608201e08d6143283b34ad54c36580e1433de87e
laurab04-design <laura_b_04@hotmail.com>
2025-06-05 00:29:01 +0100

    Update kc_breeds.py

--- kc_breeds.py
+++ kc_breeds.py
@@ -71,7 +71,7 @@ KC_BREEDS = [
 
     # Broad catchalls
     "terrier", "hound", "toy", "spaniel", "bulldog", "collie",
-    "spitz", "sheepdog", "pastoral", "working", "corgi", "pointer", "setter", "bichon", "basset", "heeler","limited"
+    "spitz", "sheepdog", "pastoral", "working", "corgi", "pointer", "setter", "bichon", "basset", "heeler","limited", "beauceron"
 ]
 
 # Deduplicate and sort for sanity
